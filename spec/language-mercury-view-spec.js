a93613c8da7a20444db18666d6f3e78b41dbbfed
tmhglnd <timohoogland@gmail.com>
2026-02-08 22:04:36 +0100

    added files for mercury syntax highlighting in pulsar

---
+++ spec/language-mercury-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LanguageMercuryView from '../lib/language-mercury-view';
+
+describe('LanguageMercuryView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
